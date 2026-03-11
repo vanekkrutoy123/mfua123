232afcf242be945efe44db6811da5f1588718b53
vanekkrutoy123 <ivan.caharov2008@gmail.com>
2026-03-11 09:13:36 +0300

    asdasd

--- Other/Bash/Scripts/main.sh
+++ Other/Bash/Scripts/main.sh
@@ -1,7 +1,6 @@
 # main.sh
 # .sh - shell
 #!/bin/bash
-<<<<<<< HEAD
 echo "Привет, Мир!"
 
 read name
@@ -51,24 +50,3 @@ echo "Сумма: $sum"
 
 
  
-=======
-echo "Привет, Мир!\n Как вас зовут?"
-read name
-echo "Привет, $name! Добро пожаловать в Bash-скрипты!"
-echo "Введите 1-е число:"
-read num1
-echo "Введите 2-е чило:"
-read num2
-
-echo "Сумма: $(($num1 + $num2))"
-echo "Разность: $(($num1 - $num2))"
-echo "Введите любое натуральное число:"
-read number
-if ((number > 10)); then
-    echo "Число > 10"
-elif ((number == 10)); then
-    echo "Число = 10"
-else
-    echo "Число < 10"
-fi
->>>>>>> 1508656d9aa5607f68f246829f6818b69e9ca4ed
